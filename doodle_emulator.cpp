2bb8d579d64521f0652dff7aa02f5aad687f0cdd
lqc <liqchuan@gmail.com>
2024-11-11 16:09:40 +0800

    未完成遮挡关系，不支持键盘输入的模拟器

--- doodle_emulator.cpp
+++ doodle_emulator.cpp
@@ -68,11 +68,13 @@ struct Keyboard
         ready = 1;
     }
 } kbd;
+
+bool shouldRender = false; // 在这个周期是否应该渲染,set in mmio_write
 int game_mode = 0;
 int game_buf = 0;
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
-std::vector<int> game_regs; // 游戏寄存器
+
 int ram[1024]; // simulate RAM
 std::vector<std::string> ascii_shape;
 int cycle = 0;
@@ -94,12 +96,218 @@ struct PictureElements
     SDL_Texture *propeller;
     SDL_Texture *propeller_running;
     SDL_Texture *doodle;
+    SDL_Texture *doodle_right;
     SDL_Texture *doodle_up;
 };
+
 PictureElements pic_elem;
+
+enum ImageType {
+    PLATFORM,
+    FAKE_PLATFORM,
+    MONSTER,
+    BALL,
+    SPRING_OR_PROPELLER,
+    DOODLE
+};
+
+enum class RegNum {
+    PLATFORM = 16,
+    FAKE_PLATFORM = 4,
+    MONSTER = 2,
+    BALL = 4,
+    SPRING_OR_PROPELLER = 4,
+    DOODLE = 1
+};
+int RegStart[6] = {0, 16, 20, 22, 26, 30}; //第i项表示第i类寄存器的在gameRegs中的起始序号，从0开始
+
+#define REGS_NUM 31
+#define PIC_NUM 14
+
+typedef struct {
+    int type; //固有属性,只在initReg里面设置
+    int content;
+    int xAddr;
+    int yAddr;
+    int num;
+    int getyAddr() {
+        return yAddr;
+    }
+
+    int getxAddr() {
+        return xAddr;
+    }
+
+    int getNum() {
+        return num;
+    }
+
+    void set(int content) {
+        this->content = content;
+        this->yAddr = content & 0x7ff;
+        this->xAddr = (content >> 11) & 0x7ff;
+        this->num = (content >> 22) & 0x3ff;
+    }
+
+} imageReg;
+
+imageReg gameRegs[REGS_NUM];
+
+typedef struct {
+    int type;
+    int num;
+    int wide;
+    int high;
+    SDL_Texture * texture;
+
+    void draw(imageReg * reg) {
+        int x = reg->getxAddr();
+        int y = reg->getyAddr();
+        SDL_Rect dest = {x, y, wide, high};
+        SDL_RenderCopy(renderer, texture, NULL, &dest);
+        SDL_RenderPresent(renderer);
+    }
+} image;
+
+image gameImgs[PIC_NUM];
+
+image * findImage(int type, int num) {
+    for (int i = 0; i < PIC_NUM; i++) {
+        if (gameImgs[i].type == type && gameImgs[i].num == num) {
+            return &gameImgs[i];
+        }
+    }
+    return NULL;
+}
+
+
+
+void initImage() {
+    gameImgs[0].type = PLATFORM;
+    gameImgs[0].num = 1;
+    gameImgs[0].wide = GREEN_PLATFORM_W;
+    gameImgs[0].high = GREEN_PLATFORM_H;
+    gameImgs[0].texture = pic_elem.green_platform;
+    gameImgs[1].type = PLATFORM;
+    gameImgs[1].num = 2;
+    gameImgs[1].wide = LIGHT_BLUE_PLATFORM_W;
+    gameImgs[1].high = LIGHT_BLUE_PLATFORM_H;
+    gameImgs[1].texture = pic_elem.light_blue_platform;
+
+    gameImgs[2].type = FAKE_PLATFORM;
+    gameImgs[2].num = 1;
+    gameImgs[2].wide = BROWN_PLATFORM_BREAKING_1_H;
+    gameImgs[2].high = BROWN_PLATFORM_BREAKING_1_H;
+    gameImgs[2].texture = pic_elem.brown_platform_breaking_1;
+    gameImgs[3].type = FAKE_PLATFORM;
+    gameImgs[3].num = 2;
+    gameImgs[3].wide = BROWN_PLATFORM_BREAKING_2_W;
+    gameImgs[3].high = BROWN_PLATFORM_BREAKING_2_H;
+    gameImgs[3].texture = pic_elem.brown_platform_breaking_2;
+
+    gameImgs[4].type = MONSTER;
+    gameImgs[4].num = 1;
+    gameImgs[4].wide = PURPLE_MONSTER_W;
+    gameImgs[4].high = PURPLE_MONSTER_H;
+    gameImgs[4].texture = pic_elem.purple_monster;
+    gameImgs[5].type = MONSTER;
+    gameImgs[5].num = 2;
+    gameImgs[5].wide = RED_MONSTER_W;
+    gameImgs[5].high = RED_MONSTER_H;
+    gameImgs[5].texture = pic_elem.red_monster;
+
+    gameImgs[6].type = BALL;
+    gameImgs[6].num = 1;
+    gameImgs[6].wide = BALL_W;
+    gameImgs[6].high = BALL_H;
+    gameImgs[6].texture = pic_elem.ball;
+
+    gameImgs[7].type = SPRING_OR_PROPELLER;
+    gameImgs[7].num = 1;
+    gameImgs[7].wide = SPRING_COMPRESSED_W;
+    gameImgs[7].high = SPRING_COMPRESSED_H;
+    gameImgs[7].texture = pic_elem.spring_compressed;
+    gameImgs[8].type = SPRING_OR_PROPELLER;
+    gameImgs[8].num = 2;
+    gameImgs[8].wide = SPRING_FULL_W;
+    gameImgs[8].high = SPRING_FULL_H;
+    gameImgs[8].texture = pic_elem.spring_full;
+    gameImgs[9].type = SPRING_OR_PROPELLER;
+    gameImgs[9].num = 3;
+    gameImgs[9].wide = PROPELLER_W;
+    gameImgs[9].high = PROPELLER_H;
+    gameImgs[9].texture = pic_elem.propeller;
+    gameImgs[10].type = SPRING_OR_PROPELLER;
+    gameImgs[10].num = 4;
+    gameImgs[10].wide = PROPELLER_RUNNING_W;
+    gameImgs[10].high = PROPELLER_RUNNING_H;
+    gameImgs[10].texture = pic_elem.propeller_running;
+
+    gameImgs[11].type = DOODLE;
+    gameImgs[11].num = 1;
+    gameImgs[11].wide = DOODLE_W;
+    gameImgs[11].high = DOODLE_H;
+    gameImgs[11].texture = pic_elem.doodle;
+    gameImgs[12].type = DOODLE;
+    gameImgs[12].num = 2;
+    gameImgs[12].wide = DOODLE_W;
+    gameImgs[12].high = DOODLE_H;
+    gameImgs[12].texture = pic_elem.doodle_right;
+    gameImgs[13].type = DOODLE;
+    gameImgs[13].num = 3;
+    gameImgs[13].wide = DOODLE_UP_W;
+    gameImgs[13].high = DOODLE_UP_H;
+    gameImgs[13].texture = pic_elem.doodle_up;
+
+}
+
+void initRegs() {
+    for(int i = 0; i < REGS_NUM; i++) {
+        gameRegs[i].set(0);
+    }
+
+    int index = 0;
+
+    // Initialize PLATFORM registers
+    for(int i = 0; i < static_cast<int>(RegNum::PLATFORM); i++, index++) {
+        gameRegs[index].type = PLATFORM;
+    }
+
+    // Initialize FAKE_PLATFORM registers
+    for(int i = 0; i < static_cast<int>(RegNum::FAKE_PLATFORM); i++, index++) {
+        gameRegs[index].type = FAKE_PLATFORM;
+    }
+
+    // Initialize MONSTER registers
+    for(int i = 0; i < static_cast<int>(RegNum::MONSTER); i++, index++) {
+        gameRegs[index].type = MONSTER;
+    }
+
+    // Initialize BALL registers
+    for(int i = 0; i < static_cast<int>(RegNum::BALL); i++, index++) {
+        gameRegs[index].type = BALL;
+    }
+
+    // Initialize SPRING_OR_PROPELLER registers
+    for(int i = 0; i < static_cast<int>(RegNum::SPRING_OR_PROPELLER); i++, index++) {
+        gameRegs[index].type = SPRING_OR_PROPELLER;
+    }
+
+    // Initialize DOODLE registers
+    for(int i = 0; i < static_cast<int>(RegNum::DOODLE); i++, index++) {
+        gameRegs[index].type = DOODLE;
+    }
+}
+
 void render_game()
 {
-    /* TODO */
+    for(int i = 0; i < REGS_NUM; i++) {
+        imageReg * reg = &gameRegs[i];
+        image * img = findImage(reg->type, reg->getNum());
+        if (img != NULL) {
+            img->draw(reg);
+        }
+    }
 }
 
 SDL_Texture *loadTexture(std::string path)
@@ -128,6 +336,7 @@ SDL_Texture *loadTexture(std::string path)
     }
     return newTexture;
 }
+
 void loadMedia()
 {
     pic_elem.green_platform = loadTexture("./image/green_platform.png");
@@ -142,8 +351,10 @@ void loadMedia()
     pic_elem.propeller = loadTexture("./image/propeller.png");
     pic_elem.propeller_running = loadTexture("./image/propeller_running.png");
     pic_elem.doodle = loadTexture("./image/doodle.png");
+    pic_elem.doodle_right = loadTexture("./image/doodle_right.png");
     pic_elem.doodle_up = loadTexture("./image/doodle_up.png");
 }
+
 SDL_Texture *texture = NULL;
 void init_asciirom()
 {
@@ -212,6 +423,9 @@ int mmio_read(int addr)
 void mmio_write(int addr, int din)
 {
     int upper = (addr >> 20) & 0xfff;
+    int typeNum = (addr >> 6) & 0x7;  //寄存器类别的序号，0是平台类寄存器
+    int typeIndex = (addr >> 2) & 0xf; //在该类别中是第几个，0表示第一个
+    int regIndex = RegStart[typeNum] + typeIndex;
     switch (upper)
     {
     case 0:
@@ -223,6 +437,8 @@ void mmio_write(int addr, int din)
         break;
     case 3:
         /* TODO */
+        gameRegs[regIndex].set(din);
+        shouldRender = true;
         break;
     case 4:
         break;
@@ -506,8 +722,6 @@ void init_mem(const char *ram_file)
         ram[size++] = std::stoll(elem, nullptr, 16);
     }
     file.close();
-    game_regs0.assign(32, 0);
-    game_regs1.assign(32, 0);
 }
 void doInput()
 {
@@ -543,12 +757,18 @@ int main(int argc, char **argv)
                                 SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
 
     loadMedia();
+    initImage();
+    initRegs();
     render_game();
     while (true)
     {
         if (!sleepstate)
         {
             cpu.eval();
+            if(shouldRender) {
+                render_game();
+                shouldRender = false;
+            }
         }
         else
         {
